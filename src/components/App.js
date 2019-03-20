e471cbee15b9f38f9b8e69838345895af78eefa1
Brian Bancroft <hello@brianbancroft.ca>
2019-03-19 22:02:41 -0400

    🚧  Add server-rendered templates and style
    *Adds it from the other app

--- src/components/App.js
+++ src/components/App.js
@@ -46,14 +46,65 @@ class App extends Component {
 
   render() {
     return (
-      <main>
-        <section>
-          <h1>Null Islanders</h1>
-        </section>
-        <section>
-          <ul>{renderPosts(this.state.posts)}</ul>
-        </section>
-      </main>
+      <>
+        <header>
+          <nav>
+            <a href="/" class="logo">
+              <div class="nav-title">
+                <div class="nav-title__icon" />
+                <div class="nav-title__title">Nullislanders</div>
+              </div>
+            </a>
+            <div class="nav-items">
+              <ul>
+                <li>
+                  <a href="/newest">new posts</a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/hashtag/gischat?f=tweets&vertical=default&src=hash&lang=en"
+                    target="_blank"
+                  >
+                    #gischat
+                  </a>
+                </li>
+                <li>
+                  <a href="https://thespatialcommunity.org" target="_blank">
+                    slack
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="nav-login">
+              <a href="/login">login</a>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <section>
+            <h1>Null Islanders</h1>
+          </section>
+          <section>
+            <ul>{renderPosts(this.state.posts)}</ul>
+          </section>
+        </main>
+        <footer>
+          <div class="footer-links-container">
+            <div class="nav-items">
+              <ul>
+                <li>
+                  <a href="/guidelines" target="_blank">
+                    Guidelines
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:hello@brianbancroft.ca">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </>
     );
   }
 }
